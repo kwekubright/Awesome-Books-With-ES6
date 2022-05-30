2139c519d807159c6247c3ba3ba10aa32c69e4a3
Kweku Bright <kweku01bright@gmail.com>
2022-05-30 20:35:08 +0100

    Fix linter errors
    Fix linter errors

--- modules/books-mod.js
+++ modules/books-mod.js
@@ -3,36 +3,36 @@ import { NavSelectors as navSelectors } from './nav-selectors-mod.js';
 
 export class BookClass {
 
-  hasBooks = localStorage.getItem('books');
-
-  books = [
-    {
-      title: 'Book 1',
-      author: 'Author 1',
-    },
-    {
-      title: 'Book 2',
-      author: 'Author 2',
-    },
-    {
-      title: 'Book 3',
-      author: 'Author 3',
-    },
-    {
-      title: 'Book 4',
-      author: 'Author 4',
-    },
-    {
-      title: 'Book 5',
-      author: 'Author 5',
-    },
-    {
-      title: 'Book 6',
-      author: 'Author 6',
-    },
-  ];
-
   constructor() {
+    this.hasBooks = localStorage.getItem('books');
+
+    this.books = [
+      {
+        title: 'Book 1',
+        author: 'Author 1',
+      },
+      {
+        title: 'Book 2',
+        author: 'Author 2',
+      },
+      {
+        title: 'Book 3',
+        author: 'Author 3',
+      },
+      {
+        title: 'Book 4',
+        author: 'Author 4',
+      },
+      {
+        title: 'Book 5',
+        author: 'Author 5',
+      },
+      {
+        title: 'Book 6',
+        author: 'Author 6',
+      },
+    ];
+
     // Lets create initial collection if there are no books in storage
     if (!this.hasBooks) {
       // Okay, now lets save books to local storage
